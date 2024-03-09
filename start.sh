871a20479e748373d720a0dc1c922ad0a825c41f
Сергей Куклин <sergeymulti@gmail.com>
2024-03-09 15:20:49 +0300

    commit3

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ if [! -d $VIRTUALENV ]; then
 fi
 
 if [! -f %VIRTUALENV/bin/pip]; then
-  curl --silent --show-err --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/
+  curl --silent --show-err --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
 
 $VIRTUALENV/bin/pip install -r requirements.txt
